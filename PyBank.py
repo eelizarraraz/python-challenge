399a0bf14a6652ea0f3d66bfa44c8099a62abf1d
eelizarraraz <eric_elizarraraz@yahoo.com>
2018-05-26 10:18:58 -0700

    PyBank

---
+++ PyBank.py
@@ -0,0 +1,8 @@
+import os
+import csv
+budget1 = ("C:/Users/eric_/Desktop/USC/python-challenge/budget_data_1.csv")
+budget2 = ("C:/Users/eric_/Desktop/USC/python-challenge/budget_data_2.csv")
+
+
+
+
